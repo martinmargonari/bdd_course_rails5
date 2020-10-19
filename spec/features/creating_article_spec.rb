47568d022f2a515b75bd8fe6fd08587c451ba19d
Martin Margonari <martinmargonari@gmail.com>
2020-10-19 07:52:15 -0300

    Link articles to users

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -17,9 +17,10 @@ RSpec.feature "Creating Articles" do
 
     click_button "Create Article"
 
+    expect(Article.last.user).to eq(@john)
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
-    expect(page).to have_content("Created by #{@john.email}")
+    expect(page).to have_content("Created by: #{@john.email}")
   end
 
   scenario "A user fails to create a new article" do
