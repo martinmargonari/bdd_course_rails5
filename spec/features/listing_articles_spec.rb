89650c62a3fe25932a644bcc75af00cb21c15bd7
Martin Margonari <martinmargonari@gmail.com>
2020-10-20 07:48:12 -0300

    Restricting access to edit and delete articles to owners

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -3,13 +3,26 @@ require "rails_helper"
 RSpec.feature "Listing Articles" do
 
   before do
-    john = User.create(email: "john@example.com", password: "password")
+    @john = User.create(email: "john@example.com", password: "password")
     login_as(@john)
-    @article1 = Article.create(title: "The first article", body: "Lorem Ipsum dolor sit amet, consectetur.", user: john)
-    @article2 = Article.create(title: "The second article", body: "Body of second article", user: john)
+    @article1 = Article.create(title: "The first article", body: "Lorem Ipsum dolor sit amet, consectetur.", user: @john)
+    @article2 = Article.create(title: "The second article", body: "Body of second article", user: @john)
   end
 
-  scenario "A user lists all articles" do
+  scenario "with articles created and users not signed in" do
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+  end
+
+  scenario "with articles created and user signed in" do
+    login_as @john
+
     visit "/"
 
     expect(page).to have_content(@article1.title)
